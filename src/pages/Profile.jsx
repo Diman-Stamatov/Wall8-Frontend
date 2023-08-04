8fb4aa2aa4a8bf541c130328ba8f816cb32c3df7
Diman_Stamatov <d.stamatov@yahoo.com>
2023-08-04 17:23:07 +0300

    Feat: updated Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,40 +4,52 @@ import { useAuth } from "../context/AuthContext";
 
 
 
+
 const Profile = () => {
     const { user } = useAuth();
     const [username, setUsername] = useState("");
     const profilePic = "https://i.kym-cdn.com/photos/images/original/002/029/752/c73.jpg"
     const verified = user.data.isVerified;
-    
-    return(
+
+    return (
         <div className="bg-dark-tertiary dark:bg-dark-primary bg-cover bg-center bg-no-repeat h-full w-full absolute top-0 left-0 z-0">
             <MainHeader username={username} setUsername={setUsername} />
-            <div className="flex items-center justify-center h-screen ">
-            
-                <div style={{fontSize:20, textAlign:'center'}} 
-                className="border border-dark-primary dark:border-light-tertiary z-10 bg-light-primary dark:bg-dark-primary w-55 px-6 py-2 rounded-md ">                    
-                    <div className="grid grid-cols-2 gap-4">
-                    <div className="col-span-2" style={{fontSize:40, textAlign:'center'}}>Welcome, {user ? user.data.username: "Guest"}.</div>                
-                    <img className="col-span-2" style = {{height:160, width:160, display: "block", margin:"auto"}}src={profilePic} alt="ProfilePic" /> 
-                    <div style={{textAlign:'left', paddingLeft:40}}>E-mail:</div>
-                    <div style={{textAlign:"left"}}>{user.data.email}</div>
-                    <div style={{textAlign:'left', paddingLeft:40}}>Phone number:</div>
-                    <div style={{textAlign:"left"}}>{user.data.phoneNumber}</div>
-                    <div style={{textAlign:'left', paddingLeft:40}}>E-mail:</div>
-                    <div style={{textAlign:"left"}}>{user.data.email}</div>
-                    <button style={{marginTop:20}} className="col-span-2  dark:bg-dark-secondary dark:hover:bg-dark-tertiary text-white font-bold py-2 px-4 rounded">
-                    Update your phone number
-                    </button>
-                    <button style={{marginBottom:20}} className="col-span-2   dark:bg-dark-secondary dark:hover:bg-dark-tertiary text-white font-bold py-2 px-4 rounded">
-                    Update your e-mail
-                    </button>
-                    
+            <div style={{ marginTop: 50 }} className="flex justify-center ">
+                <div style={{ fontSize: 20, textAlign: 'center' }}
+                    className="  border border-dark-primary dark:border-light-tertiary z-10 bg-light-primary dark:bg-dark-primary w-55 px-6 py-2 rounded-md ">
+                    <table className="table-auto border-separate border-spacing-2 ">
+                        <tbody>
+                            <tr >
+                                <td style={{ fontSize: 40 }} colSpan={3}>Welcome, {user ? user.data.username : "Guest"}.</td>
+                            </tr>
+                            <tr >
+                                <td colSpan={3}>
+                                    <img style={{ height: 160, width: 160, display: "block", margin: "auto", marginBottom:40}} src={profilePic} alt="ProfilePic" />
+                                </td>
+                            </tr>
+                            <tr >
+                                <td style={{ textAlign: "left", paddingLeft: 20 }}>E-mail:</td>
+                                <td>{user ? user.data.email : "some@other.com"}</td>
+                                <td className="  button  dark:bg-dark-secondary dark:hover:bg-dark-tertiary text-white font-bold  px-4 rounded">
+                                    Update
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style={{ textAlign: "left", paddingLeft: 20 }}>Phone number:</td>
+                                <td>{user ? user.data.phoneNumber : "0888 890 123"}</td>
+                                <td className="button  dark:bg-dark-secondary dark:hover:bg-dark-tertiary text-white font-bold  px-1 rounded">
+                                    Update
+                                </td>
+                            </tr>
+                            <tr >
+                                <td style={{ paddingTop: 40 }} className="max-w-md" colSpan={3}>{!verified ? "You have not verified your account yet! Please check your inbox for the verification link sent to you after registering your account." : ""}</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
-    </div>
-    ); 
+    );
 };
 
 export default Profile;
