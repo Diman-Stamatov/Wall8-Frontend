cbc12b88891cebf27b0c230d65b01156df2944fa
Iliyan Tsvetkov <iliancetkov@gmail.com>
2023-08-05 17:47:25 +0300

    feat: finish transfer first component;

--- src/components/transfers/transferprocess/TransferPickRecipient.jsx
+++ src/components/transfers/transferprocess/TransferPickRecipient.jsx
@@ -38,24 +38,24 @@ const TransferPickRecipient = ({
   return (
     <div style={{ width: "350px" }}>
       <h1 className="text-4xl text-center mb-4">Select a recipient</h1>
-      <div className="flex justify-between items-center mb-4"></div>
-      <input
-        className="border border-gray-300 dark:border-dark-tertiary rounded-lg px-3 py-2 w-full mb-2 focus:outline-none focus:ring focus:border-blue-300 dark:focus:border-dark-primary"
-        id="search"
-        type="text"
-        placeholder="Search..."
-        value={filterText}
-        onChange={(e) => onFilterTextChange(e.target.value)}
-      />
-
-      <div className="border-2 shadow-inner dark:shadow-light-quaternary rounded-lg pt-4 pb-4 px-2 py-2 mb-4">
+      <div className="focus-within:shadow-lg flex flex-row justify-start max-w-md mb-3">
+        <input
+          className="transfer-input w-full px-4 py-2 rounded-xl shadow-sm dark:shadow-light-primary dark:bg-inherit focus:outline-none focus:ring-1 focus:ring-inherit focus:border-inherit"
+          type="text"
+          placeholder="Search..."
+          value={filterText}
+          onChange={(e) => onFilterTextChange(e.target.value)}
+          style={{ transition: "all .15s ease" }}
+        />
+      </div>
+      <div className="border-2 shadow-inner dark:shadow-light-quaternary rounded-lg pt-4 pb-4 px-2 py-2 ">
         <ul className="space-y-2">
           {filteredRecipients.map((filtRec, index) => (
             <li
               key={index}
               className={` border rounded-lg px-4 py-2 shadow-inner dark:shadow-light-quaternary ${
                 openIndex !== index
-                  ? "hover:dark:bg-dark-primary cursor-pointer"
+                  ? "hover:dark:bg-dark-primary cursor-pointer hover:translate-x-1"
                   : ""
               } dark:text-light-primary`}
             >
@@ -63,7 +63,8 @@ const TransferPickRecipient = ({
                 className="flex justify-between items-center"
                 onClick={toggleOpen(index)}
               >
-                <p className="cursor-pointer flex items-center ">
+                {openIndex === index ? "" : <Avatar />}
+                <p className="cursor-pointer text-lg flex  ">
                   {filtRec.username}
                   {recipient.id === filtRec.id && (
                     <AiOutlineCheck className="ml-2 cursor-pointer text-green-500" />
@@ -94,7 +95,7 @@ const TransferPickRecipient = ({
                   </p>
                   <div className="flex justify-end">
                     <button
-                      className="border dark:border-light-primary rounded-lg px-2 py-1 hover:bg-gray-100 dark:hover:bg-dark-secondary dark:hover:border-dark-secondary cursor-pointer"
+                      className="border  dark:border-light-primary rounded-lg px-2 py-1 hover:translate-x-0.5 hover:bg-gray-100 dark:hover:bg-dark-secondary dark:hover:border-dark-secondary cursor-pointer"
                       onClick={() => selectRecipient(filtRec)}
                     >
                       <p className="dark:text-light-primary ">Select</p>
@@ -106,15 +107,20 @@ const TransferPickRecipient = ({
           ))}
         </ul>
       </div>
-      <button
-        className="text-sm text-blue-500 hover:underline focus:outline-none"
-        onClick={clearRecipient}
-      >
-        Clear selection
-      </button>
-      <button onClick={onNext} className="border py-1 px-4 rounded-md m-2">
-        Next
-      </button>
+      <div className="flex-row justify-between flex mt-2">
+        <button
+          className="text-xl hover:text-light-tertiary focus:outline-none"
+          onClick={clearRecipient}
+        >
+          Clear selection
+        </button>
+        <button
+          onClick={onNext}
+          className="border py-1 px-4 text-center hover:bg-dark-primary hover:translate-x-0.5 rounded-full font-semibold"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
