868540fa3894c1a8e24f6e7a8d2a1ebf625803e9
Moloh <konstantinstoianov313@gmail.com>
2023-08-05 19:11:39 +0300

    Fix(UserProfileTab): more centering

--- src/components/profile/UserProfileTab.jsx
+++ src/components/profile/UserProfileTab.jsx
@@ -13,15 +13,15 @@ function UserProfileTab({ balance }) {
         console.log("Logout finished");
     }
     return (
-        <div class="mx-auto right-0 mt-2 w-60">
-            <div class="dark:bg-light-primary rounded overflow-hidden shadow-lg outline dark:outline-light-tertiary">
-                <div class="text-center p-6 dark:bg-dark-primary border-b">
-                    <svg aria-hidden="true" role="img" class="h-24 w-24 dark:text-light-primary rounded-full mx-auto" width="32" height="32" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256"><path fill="currentColor" d="M172 120a44 44 0 1 1-44-44a44 44 0 0 1 44 44Zm60 8A104 104 0 1 1 128 24a104.2 104.2 0 0 1 104 104Zm-16 0a88 88 0 1 0-153.8 58.4a81.3 81.3 0 0 1 24.5-23a59.7 59.7 0 0 0 82.6 0a81.3 81.3 0 0 1 24.5 23A87.6 87.6 0 0 0 216 128Z"></path></svg>
-                    <p class="pt-2 text-lg font-semibold dark:text-light-primary">John Doe</p>
-                    <p class="text-sm dark:text-light-primary">John@Doe.com</p>
-                    <div class="mt-5 ">
+        <div className="mx-auto right-0 mt-2 w-60">
+            <div className="dark:bg-light-primary rounded overflow-hidden shadow-lg outline dark:outline-light-tertiary">
+                <div className="text-center p-6 dark:bg-dark-primary border-b">
+                    <svg aria-hidden="true" role="img" className="h-24 w-24 dark:text-light-primary rounded-full mx-auto" width="32" height="32" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256"><path fill="currentColor" d="M172 120a44 44 0 1 1-44-44a44 44 0 0 1 44 44Zm60 8A104 104 0 1 1 128 24a104.2 104.2 0 0 1 104 104Zm-16 0a88 88 0 1 0-153.8 58.4a81.3 81.3 0 0 1 24.5-23a59.7 59.7 0 0 0 82.6 0a81.3 81.3 0 0 1 24.5 23A87.6 87.6 0 0 0 216 128Z"></path></svg>
+                    <p className="pt-2 text-lg font-semibold dark:text-light-primary">John Doe</p>
+                    <p className="text-sm dark:text-light-primary">John@Doe.com</p>
+                    <div className="mt-5 ">
                         <Link to="/profile">
-                            <button class="hover:dark:bg-dark-secondary border dark:border-light-primary rounded-full dark:bg-dark-primary  py-2 px-4 text-xs font-semibold dark:text-light-primary"
+                            <button className="hover:dark:bg-dark-secondary border dark:border-light-primary rounded-full dark:bg-dark-primary  py-2 px-4 text-xs font-semibold dark:text-light-primary"
                             >
                                 Manage your Account
                             </button>
@@ -29,11 +29,11 @@ function UserProfileTab({ balance }) {
                     </div>
                 </div>
 
-                <div class="border-b dark:bg-dark-primary">
+                <div className="border-b dark:bg-dark-primary">
                     <WalletBalance balance={balance} />
                 </div>
 
-                <div class=" dark:bg-dark-primary text-center">
+                <div className=" dark:bg-dark-primary text-center">
                     <ConfirmDialog
                         buttonName="Logout"
                         title="Confirm Logout"
