6feb39d298f7302691d4d2f16150725ba49e626c
D1manko <d.stamatov@yahoo.com>
2023-08-06 18:32:19 +0300

    Fix: ChangeEmail token as query paramater

--- src/pages/UpdateEmailPage.jsx
+++ src/pages/UpdateEmailPage.jsx
@@ -4,7 +4,7 @@ import EmailForm from "../components/profile/EmailForm";
 
 import axios from "axios";
 
-const UpdateEmailPage = () => {  
+const UpdateEmailPage = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const handleConfirm = async (values, { setSubmitting, resetForm }) => {
@@ -15,18 +15,17 @@ const UpdateEmailPage = () => {
       const response = await axios.put(
         "http://localhost:5120/api/virtual-wallet/users/change-email",
         {
-          params:
-          {
-            token:values.token
-          },
           email: values.email,
-          confirmEmail: values.confirmEmail
-        },        
-        { 
-          withCredentials: true 
+          confirmEmail: values.confirmEmail,
+        },
+        {
+          params: {
+            token: values.token,
+          },
+          withCredentials: true,
         }
       );
-      console.log("change email response: ", response);      
+      console.log("change email response: ", response);
       setIsRegistered(true);
       resetForm();
     } catch (error) {
@@ -38,20 +37,19 @@ const UpdateEmailPage = () => {
   const validationSchema = Yup.object().shape({
     token: Yup.string().required("Required"),
     email: Yup.string().email("Invalid email").required("Required"),
-    confirmEmail: Yup.string().required("Required").oneOf(
-      [Yup.ref("email"), null],
-      "E-mails must match"
-    ),
+    confirmEmail: Yup.string()
+      .required("Required")
+      .oneOf([Yup.ref("email"), null], "E-mails must match"),
   });
 
   return (
-        <div className="dark:bg-dark-primary bg-cover bg-center bg-no-repeat h-full w-full absolute top-0 left-0 z-0">
-          <EmailForm
-            validationSchema={validationSchema}
-            handleConfirm={handleConfirm}
-            isLoading={isLoading}
-          />
-        </div>
+    <div className="dark:bg-dark-primary bg-cover bg-center bg-no-repeat h-full w-full absolute top-0 left-0 z-0">
+      <EmailForm
+        validationSchema={validationSchema}
+        handleConfirm={handleConfirm}
+        isLoading={isLoading}
+      />
+    </div>
   );
 };
 
