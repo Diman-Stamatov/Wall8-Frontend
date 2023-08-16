f0f32587bf218a420311e091c7e0e3d6da91a105
Iliyan Tsvetkov <iliancetkov@gmail.com>
2023-08-17 00:05:43 +0300

    donttouchmystuffmfb: fixed usersection guy

--- src/components/profile_tab/UserSection.jsx
+++ src/components/profile_tab/UserSection.jsx
@@ -8,20 +8,23 @@ function UserSection() {
   const { username, email, photoUrl } = user.data;
 
   return (
-    <div className=" shadow-light-quaternary dark:bg-gradient-to-t dark:text-light-primary dark:from-dark-primary dark:to-light-quaternary border  dark:border-light-secondary mt-5 flex justify-between rounded-xl shadow-2xl px-2 py-10 mb-2">
+    <div className="relative shadow-light-quaternary dark:bg-gradient-to-t dark:text-light-primary dark:from-dark-primary dark:to-light-quaternary border  dark:border-light-secondary mt-5 flex justify-between rounded-xl shadow-2xl px-2 py-10 mb-2">
       <Avatar
         src={photoUrl}
-        className="shadow-md dark:shadow-light-secondary m-auto"
+        className="shadow-md dark:shadow-light-secondary ml-3"
         sx={{ width: "180px", height: "180px" }}
       />
       <div className="self-center m-auto">
-        <h1 className="font-semibold dark:text-light-primary ">{username}</h1>
-        <p className=" text-center text-sm font-semibold">{email}</p>
-        <Link to={`profile/${username}`} className="self-end text-center">
-        <p>account settings</p>
-      </Link>
+        <h1 className="font-semibold text-4xl  dark:text-light-primary ">
+          {username}
+        </h1>
+        <p className=" ml-2 text-sm dark:text-dark-tertiary font-semibold mt-2">{email}</p>
       </div>
-      
+      <div className="absolute bottom-0 right-0 p-2">
+        <Link to={`profile/${username}`}>
+          <p className="uppercase text-xs dark:hover:text-dark-tertiary dark:text-dark-secondary font-black">account settings</p>
+        </Link>
+      </div>
     </div>
   );
 }
