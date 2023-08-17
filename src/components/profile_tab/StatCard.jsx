4212675be363cded72c9471df9406f9889c1b62b
Iliyan Tsvetkov <iliancetkov@gmail.com>
2023-08-17 15:11:13 +0300

    feat: add spinning button, stat cards done + other small design amend.;

--- src/components/profile_tab/StatCard.jsx
+++ src/components/profile_tab/StatCard.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 
-function StatCard() {
+function StatCard({ header, body, dates }) {
   return (
     <div
       style={{ width: "240px", height: "145px" }}
       className="rounded-lg  shadow-2xl shadow-light-quaternary dark:shadow-dark-secondary dark:bg-gradient-to-b dark:from-dark-secondary dark:to-dark-primary transition-transform transform hover:scale-110 hover:z-10"
     >
       <div className="border-b-2 border-success px-3 py-1 dark:text-light-primary font-semibold ">
-        Income
+        {header}
       </div>
       <div className="p-2">
-        <h5 className="mb-2 text-xl font-medium leading-tight text-success dark:text-light-primary">
-          $49394
+        <h5
+          className={`mb-2 text-xl font-medium leading-tight text-success ${
+            header.toLowerCase().includes("sent") ? "text-red-600" : "text-green-600"
+          }`}
+        >
+          {body}
         </h5>
-        <p className="text-base dark:text-light-primary">+ $345 from last week</p>
       </div>
       <div className="text-sm border-t-2 border-success px-4 py-auto mt-2 dark:text-light-primary">
-        9 - 16 August
+        {dates}
       </div>
     </div>
   );
--- src/components/profile_tab/StatsSection.jsx
+++ src/components/profile_tab/StatsSection.jsx
@@ -1,24 +1,121 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StatCard from "./StatCard";
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import axios from "axios";
+
+import RefreshButton from "../RefreshButton";
+import { useUserLocale } from "../../context/LocaleContext";
+
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const StatsSection = () => {
+  const { userLocale } = useUserLocale();
+  const { user } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const [incomeBody, setIncomeBody] = useState(null);
+  const [dates, setDates] = useState(null);
+  const [sentBody, setSentBody] = useState(null);
+  const [sentDates, setSentDates] = useState(null);
+  const [transfersSent, setTransfersSent] = useState(null);
+  const [transfersReceived, setTransfersReceived] = useState(null);
+
+  const formatToISO8601 = (date) => {
+    return date.toISOString().slice(0, 19) + "Z";
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    const today = new Date();
+    const oneWeekAgo = new Date(today);
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+    const start = formatToISO8601(oneWeekAgo);
+    const end = formatToISO8601(today);
+    try {
+      await axios
+        .get(
+          `http://localhost:5120/api/virtual-wallet/users/${user.data.id}/statistics`,
+          {
+            params: {
+              startDate: start,
+              endDate: end,
+            },
+            withCredentials: true,
+          }
+        )
+        .then((response) => {
+          setIncomeBody(
+            new Intl.NumberFormat(userLocale, {
+              style: "currency",
+              currency: response.data.currency,
+            }).format(response.data.amountReceived)
+          );
+          setSentBody(
+            new Intl.NumberFormat(userLocale, {
+              style: "currency",
+              currency: response.data.currency,
+            }).format(response.data.amountSent)
+          );
+          setTransfersSent(response.data.transfersSent);
+          setTransfersReceived(response.data.transfersReceived);
+          const endDate = new Date(response.data.endDate);
+          const startDate = new Date(response.data.startDate);
+          const endDay = endDate.getDate();
+          const startDay = startDate.getDate();
+          const monthIndex = endDate.getMonth();
+          const month = MONTHS[monthIndex];
+          setDates(`${startDay} - ${endDay} ${month}`);
+        });
+    } catch (error) {
+      console.log("STATS ERROR:", error);
+    }
+    setIsLoading(false);
+  };
   return (
     <div className="px-5 flex flex-col mt-5 ">
-      <div>
-      <h1 className="ml-5 font-medium text-center text-2xl dark:text-light-primary mb-5">Statistics</h1>
+      <div className="flex ">
+        <h1 className="ml-5 font-medium text-center text-2xl dark:text-light-primary mb-5">
+          Statistics
+        </h1>
+        <RefreshButton isLoading={isLoading} fetchData={fetchData} />
       </div>
       <div className="flex space-x-11 px-5">
-        
         <div
           className="grid grid-cols-2 gap-10 pr-6"
           style={{ width: "500px", height: "360px" }}
         >
-          <StatCard />
-          <StatCard />
-          <StatCard />
-          <StatCard />
+          <StatCard
+            header="Received (last week)"
+            body={incomeBody}
+            dates={dates}
+          />
+          <StatCard header="Sent (last week)" body={sentBody} dates={dates} />
+          <StatCard
+            header="Transfers sent (last week)"
+            body={transfersSent}
+            dates={dates}
+          />
+          <StatCard
+            header="Transfers received (last week)"
+            body={transfersReceived}
+            dates={dates}
+          />
         </div>
         {/* Big Button */}
         <div className="flex flex-col justify-center items-center p-4 ml-4">
