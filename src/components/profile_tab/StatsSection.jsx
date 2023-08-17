f2666e59717476474cabef09cec70c84d213df3a
Diman_Stamatov <d.stamatov@yahoo.com>
2023-08-17 14:26:26 +0300

    Feat: Blocked user UI changes

--- src/components/profile_tab/StatsSection.jsx
+++ src/components/profile_tab/StatsSection.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import StatCard from "./StatCard";
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import { Box } from "@mui/system";
 
 const StatsSection = () => {
+
+  const { user } = useAuth();
+
   return (
     <div className="px-5 flex flex-col mt-5 ">
       <div>
@@ -21,8 +26,10 @@ const StatsSection = () => {
           <StatCard />
         </div>
         {/* Big Button */}
+        
         <div className="flex flex-col justify-center items-center p-4 ml-4">
-          <Link to={`transfer`}>
+        {!user.data.isBlocked?
+          <><Link to={`transfer`}>
             <PaperAirplaneIcon
               className="ml-20 hover:scale-y-150 cursor-pointer hover:-translate-y-2 h-12  w-12 text-blue-500"
               style={{ width: "120px", height: "120px" }}
@@ -31,7 +38,14 @@ const StatsSection = () => {
           <p className="mt-2 ml-20 text-center font-extrabold text-3xl dark:text-light-primary">
             Transfer
           </p>
-        </div>
+          </> : 
+          <div className="rounded-lg  shadow-2xl shadow-light-quaternary dark:text-light-primary dark:bg-gradient-to-b
+           dark:from-dark-secondary dark:to-dark-primary"
+          style={{width: "210px", height: "200px", textAlign:"justify", padding:10}}>
+            Your account has been <span style={{color:"red", fontWeight: 'bold'}}>blocked </span> 
+            and can therefore not send or receive any transfers. 
+            Please contact our support team for any further questions regarding this issue.</div>}
+        </div> 
       </div>
     </div>
   );
